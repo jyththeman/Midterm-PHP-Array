b11ad1391348253cf2419ed7d06869bdc033d92b
jyththeman <jythgerundio@gmail.com>
2019-02-04 14:43:58 +0800

    added a test array

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    
+<?php  
+    $ppl = array(
+        "Peter"=>"35",
+        "Ben"=>"37",
+        "Joe"=>"43",
+    );
+    
+    asort($ppl);
+    
+    foreach($ppl as $name => $age){
+        echo "Key=$name, value=$age"."<br>";
+    }
+    
+?>  
+</body>
+</html>
